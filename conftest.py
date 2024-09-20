0b311eac16579e1e9e0d4a3ec902cbe5f0e0e673
Артем Ямщиков <a.yamshchikov@adept.ru>
2024-09-20 12:35:41 +0300

    add confest

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help="Choose language: en or fr or es")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_argument(f"--lang={user_language}")
+
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=options)
+
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
