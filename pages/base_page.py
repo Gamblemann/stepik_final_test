2c9912b6e43e4cf4f072678bc9f8e411e685206e
Gamblemann <raiderlook@gmail.com>
2024-09-24 13:36:53 +0300

    проверка элемента на странице и добавление assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,15 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(10)
     def open(self):
         self.browser.get(self.url)
+    def is_element_present(self, by, locator):
+        try:
+            self.browser.find_element(by, locator)
+        except (NoSuchElementException):
+            return False
+        return True
